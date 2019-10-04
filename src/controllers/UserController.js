53696694ec823a9290648b9c72d4fb381534c4f3
Guilherme Oliveira <gmbo182@hotmail.com>
2019-10-04 15:06:32 -0300

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,27 +1,27 @@
 const User = require('./../models/User');
 
-module.exports = {
-    async save(req, res) {
-        const newUserData = {  
-            nome: req.body.nome,  
-            email: req.body.cpf,
-            idade: req.body.idade  
-        };
+const save = async (req, res) => {
+    const newUserData = {  
+        nome: req.body.nome,  
+        email: req.body.cpf,
+        idade: req.body.idade  
+    };
 
-        const userFromDb = await User.findOne({ cpf: newUserData.cpf });
-  
-        return userFromDb 
-            ? res.json({ "message": "Usuário já cadastrado!"}) 
-            : res.json(await User.create(newUserData));
-    },
+    const userFromDb = await User.findOne({ cpf: newUserData.cpf });
 
-    async find(req, res) {
-        return res.json(await User.find());
-    },
-
-    async findOne(req, res) {
-        await User.findById(req.params.id, (err, user) => {
-           return (err) ? res.json({ "message": `Usuário não encontrado para o ${req.params.id} !` }) : res.json(user);
-        });
-    }
+    return userFromDb 
+        ? res.json({ "message": "Usuário já cadastrado!"}) 
+        : res.json(await User.create(newUserData));
 }
+
+const find = async (req, res) => {
+    return res.json(await User.find());
+}
+
+const findOne = async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if(!user) return res.json({ "message": `Usuário não encontrado para o ${req.params.id} !` });
+    return res.json(user);
+}
+
+module.exports = { save, find, findOne }
